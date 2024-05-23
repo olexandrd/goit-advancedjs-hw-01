ebfa108f8b1e07b9bc92708426974aaf1231eb34
Olexandrd <olexandrd@gmail.com>
2024-05-23 22:51:32 +0300

    Task 3 fixes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,27 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 const input = form.querySelector('input');
 const textarea = form.querySelector('textarea');
-const formData = {};
 
-loadSavedData();
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
+
+input.value = formData.email ?? '';
+textarea.value = formData.message ?? '';
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormInput(event) {
   formData[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
   console.log(formData);
   event.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-}
-
-function loadSavedData() {
-  const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
-  if (savedMessage) {
-    input.value = savedMessage.email;
-    textarea.value = savedMessage.message;
-  }
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
 }
